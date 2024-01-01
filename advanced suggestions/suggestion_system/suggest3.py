74887c8d11c8f53a789cabd6fb57c6e9b6f88321
i0xiis <d3bil3khihi@gmail.com>
2024-01-01 14:01:55 +0100

    FINISHED DELETE MODAL

--- advanced suggestions/suggestion_system/suggest3.py
+++ advanced suggestions/suggestion_system/suggest3.py
@@ -315,6 +315,116 @@ class Deny(ui.Modal):
 
             await db.commit()
 
+    #! DELETE MODAL #
+
+class Delete(ui.Modal):
+
+    def __init__(self):
+        super().__init__(title = "Delete a suggestion!")
+
+    sugg_id = ui.TextInput(label = "Suggestion ID:", style = discord.TextStyle.short, placeholder = "Enter suggestion ID here", required = True)
+    mod_comment = ui.TextInput(label = "Mod comment:", style = discord.TextStyle.long, placeholder = "Add your mod comment here (optional)", required = False, max_length = 500)
+
+    async def on_submit(self, interaction: discord.Interaction):
+
+        sugg_id = self.sugg_id.value
+        mod_comment = self.mod_comment.value
+        s_id = int(sugg_id)
+
+        async with aiosqlite.connect("suggestions.db") as db:
+            async with db.cursor() as cursor:
+                await cursor.execute("SELECT author, title, suggestion, msg_id, log_id FROM suggestions WHERE id = ? AND guild = ?", (s_id, interaction.guild.id))
+                data = await cursor.fetchone()
+                if data:
+
+                    author = data[0]
+                    title = data[1]
+                    suggestion = data[2]
+                    msg_id = data[3]
+                    log_id = data[4]
+
+                        #* Log message #
+
+                    delete = discord.Embed(
+                        title = "Suggestion log",
+                        description = f"Suggestion log of <@{author}>",
+                        color = discord.Color.dark_blue(),
+                        timestamp = datetime.datetime.utcnow()
+                    )
+                    delete.add_field(
+                        name = ":id: - Suggestion ID:",
+                        value = f"```{s_id}```",
+                        inline = False
+                    )
+                    delete.add_field(
+                        name = ":pencil: - Name of suggetion:",
+                        value = title,
+                        inline = False
+                    )
+                    delete.add_field(
+                        name = ":bulb: - Suggestion:",
+                        value = suggestion,
+                        inline = False
+                    )
+                    delete.add_field(
+                        name = f"{trashcanemoji} - Deleted:",
+                        value = f"This suggestion has been deleted by {interaction.user.mention}!",
+                        inline = False
+                    )
+                    if mod_comment:
+                        delete.add_field(
+                            name = f":speech_balloon: - Mod comment:",
+                            value = mod_comment,
+                            inline = False
+                        )
+                    else:
+                        delete.add_field(
+                            name = f":speech_balloon: - Mod comment:",
+                            value = "-",
+                            inline = False
+                        )
+                    delete.set_thumbnail(url = trashcanemojilink)
+
+                    suggestlog = interaction.client.get_channel(approval_channel)
+                    log_msg = await suggestlog.fetch_message(log_id)
+                    
+                    await log_msg.edit(embed = delete)
+
+                    await cursor.execute("UPDATE suggestions SET status = ?, mod = ?, mod_comment = ? WHERE id = ? AND guild = ?", ("Deleted", interaction.user.id, mod_comment, s_id, interaction.guild.id))
+
+                        #* Suggestion messaage #
+
+                    suggestmsg = interaction.client.get_channel(suggest_channel)
+                    sugg_msg = await suggestmsg.fetch_message(msg_id)
+
+                    await sugg_msg.delete()
+
+                        #* Confirm message #
+                    
+                    embed = discord.Embed(
+                        title = f"Suggestion deleted!",
+                        description = f"Suggestion with ID {s_id} has been deleted.",
+                        color = discord.Color.dark_blue(),
+                        timestamp = datetime.datetime.utcnow()
+                    )
+                    embed.set_thumbnail(url = yesemojilink)
+                    await interaction.response.send_message(embed = embed, ephemeral = True)
+
+                else:
+
+                        #* ID not found #
+                    
+                    embed = discord.Embed(
+                        title = f"Suggestion approve",
+                        description = f"ID {s_id} not found..",
+                        color = discord.Color.red(),
+                        timestamp = datetime.datetime.utcnow()
+                    )
+                    embed.set_thumbnail(url = noemojilink)
+                    await interaction.response.send_message(embed = embed, ephemeral = True)
+
+            await db.commit()
+
 class ApprovalSystem(discord.ui.View):
     def __init__(self):
         super().__init__(timeout = None)
@@ -325,7 +435,7 @@ class ApprovalSystem(discord.ui.View):
 
         self.Approve.disabled = True 
         self.Deny.disabled = True
-        # self.Delete.disabled = True
+        self.Delete.disabled = True
 
         await interaction.response.send_modal(Approve())
         await interaction.message.edit(view = self)
@@ -335,45 +445,20 @@ class ApprovalSystem(discord.ui.View):
 
         self.Approve.disabled = True
         self.Deny.disabled = True
-        # self.Delete.disabled = True
+        self.Delete.disabled = True
 
         await interaction.response.send_modal(Deny())
         await interaction.message.edit(view = self)
 
-    # @discord.ui.button(label = "Delete", style = discord.ButtonStyle.red, emoji = trashcanemoji, custom_id = "delete")
-    # async def Delete(self, interaction: discord.Interaction, Button: discord.Button):
+    @discord.ui.button(label = "Delete", style = discord.ButtonStyle.red, emoji = trashcanemoji, custom_id = "delete")
+    async def Delete(self, interaction: discord.Interaction, Button: discord.Button):
 
-    #     self.Approve.disabled = True 
-    #     self.Deny.disabled = True 
-    #     self.Delete.disabled = True
+        self.Approve.disabled = True 
+        self.Deny.disabled = True 
+        self.Delete.disabled = True
 
-    #     delete = discord.Embed(
-    #         title = "Suggestion log",
-    #         description = f"Suggestion log of {self.author.mention}",
-    #         color = discord.Color.dark_blue(),
-    #         timestamp = datetime.datetime.utcnow()
-    #     )
-    #     delete.add_field(
-    #         name = ":pencil: - Name of suggetion:",
-    #         value = self.title,
-    #         inline = False
-    #     )
-    #     delete.add_field(
-    #         name = ":bulb: - Suggestion:",
-    #         value = self.suggestion,
-    #         inline = False
-    #     )
-    #     delete.add_field(
-    #         name = f"{trashcanemoji} - Deleted:",
-    #         value = f"This suggestion has been deleted by {interaction.user.mention}!",
-    #         inline = False
-    #     )
-    #     delete.set_thumbnail(url = trashcanemojilink)
-
-    #     await self.suggmsg.delete()
-
-    #     await interaction.response.edit_message(embed = delete, view = self)
-
+        await interaction.response.send_modal(Delete())
+        await interaction.message.edit(view = self)
 
 class suggest(commands.Cog):
     def __init__(self, client: commands.Bot):
